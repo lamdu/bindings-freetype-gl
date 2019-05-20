d1f613a0be4c6f317a6413001975b41cc9ca9702
Sean Chalmers <sclhiannan@gmail.com>
2019-05-20 14:20:39 +1000

    remove git command from setup hs, set build type back to simple

--- Setup.hs
+++ Setup.hs
@@ -1,14 +1,2 @@
 import Distribution.Simple
-import Distribution.Verbosity
-import Distribution.Simple.Utils
-
-prepareBuild =
-    rawSystemExit normal "git" ["submodule", "update", "--init", "--recursive"]
-
-main =
-    defaultMainWithHooks simpleUserHooks
-    { preBuild = \args buildFlags ->
-          do
-              prepareBuild
-              preBuild simpleUserHooks args buildFlags
-    }
+main = defaultMain
