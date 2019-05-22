c178be550fbffe135435478a71fcb1dad537963a
Yair Chuchem <yairchu@gmail.com>
2019-05-22 12:53:57 +0300

    Attempt to fix nix-based build
    (without breaking stack-based build)

--- Setup.hs
+++ Setup.hs
@@ -1,9 +1,21 @@
 import Distribution.Simple
 import Distribution.Verbosity
 import Distribution.Simple.Utils
+import System.Directory (doesFileExist)
 
 prepareBuild =
-    rawSystemExit normal "git" ["submodule", "update", "--init", "--recursive"]
+    do
+        ok <- doesFileExist "freetype-gl/CMakeLists.txt"
+        if ok
+            then
+                -- Nix based builds automatically update the submodules,
+                -- so updating them is not necessary.
+                -- More-over, they fail if finding a .git folder,
+                -- hence avoiding invocations of git is necessary.
+                return ()
+            else
+                -- Stack/cabal based builds require updating the submodules
+                rawSystemExit normal "git" ["submodule", "update", "--init", "--recursive"]
 
 main =
     defaultMainWithHooks simpleUserHooks
