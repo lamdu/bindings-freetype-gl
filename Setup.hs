809c1bde479059ca8e239ab0ae3eb8b398407f31
Yair Chuchem <yairchu@gmail.com>
2019-06-18 16:11:04 +0300

    Workaround stack bug
    Couldn't be used as dependency because Stack 2.1.1 has problem with submodules.
    This change should allow to use the workaround described in
    https://github.com/commercialhaskell/stack/issues/4865#issuecomment-502169845

--- Setup.hs
+++ Setup.hs
@@ -1,26 +1,29 @@
-import Distribution.Simple
 import Distribution.Verbosity
+import Distribution.Simple
+import Distribution.Simple.Setup
 import Distribution.Simple.Utils
 import System.Directory (doesFileExist)
 
-prepareBuild =
+prepareBuild :: Verbosity -> IO ()
+prepareBuild verbosity =
     do
         ok <- doesFileExist "freetype-gl/CMakeLists.txt"
         if ok
             then
-                -- Nix based builds automatically update the submodules,
+                -- Nix and future versions of Stack automatically update the submodules,
                 -- so updating them is not necessary.
                 -- More-over, they fail if finding a .git folder,
                 -- hence avoiding invocations of git is necessary.
                 return ()
             else
-                -- Stack/cabal based builds require updating the submodules
-                rawSystemExit normal "git" ["submodule", "update", "--init", "--recursive"]
+                -- Stack/cabal based builds currently require updating the submodules
+                rawSystemExit verbosity "base" ["prepare_submodule.sh"]
 
 main =
     defaultMainWithHooks simpleUserHooks
     { preBuild = \args buildFlags ->
-          do
-              prepareBuild
-              preBuild simpleUserHooks args buildFlags
+        do
+            let verbosity = fromFlagOrDefault normal $ buildVerbosity buildFlags
+            prepareBuild verbosity
+            preBuild simpleUserHooks args buildFlags
     }
