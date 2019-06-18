6efdced2d0867988a0a008145ae2c9a7bf69bb94
Yair Chuchem <yairchu@gmail.com>
2019-06-18 16:22:10 +0300

    another attempt at working around stack bug

--- prepare_submodule.sh
+++ prepare_submodule.sh
@@ -4,6 +4,7 @@ git submodule update --init --recursive && exit 0
 
 wget https://github.com/lamdu/freetype-gl/archive/d5bc473468422f2cc536bab6e6a87a9aabb7ea07.tar.gz
 tar xvzf d5bc473468422f2cc536bab6e6a87a9aabb7ea07.tar.gz
+mkdir -p freetype-gl
 mv freetype-gl-d5bc473468422f2cc536bab6e6a87a9aabb7ea07/* freetype-gl/
 rm d5bc473468422f2cc536bab6e6a87a9aabb7ea07.tar.gz
 rm -r freetype-gl-d5bc473468422f2cc536bab6e6a87a9aabb7ea07
