abcb891cf2ed01edb2d02197ef2b354d6ff43db5
Eyal Lotem <eyal.lotem@gmail.com>
2019-06-20 23:51:52 +0300

    git submodule update is a must if .git exists

--- prepare_submodule.sh
+++ prepare_submodule.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-git submodule update --init --recursive && exit 0
+set -eu
+
+if [ -e .git ]
+then
+    git submodule update --init --recursive
+    exit 0
+fi
 
 wget https://github.com/lamdu/freetype-gl/archive/d5bc473468422f2cc536bab6e6a87a9aabb7ea07.tar.gz
 tar xvzf d5bc473468422f2cc536bab6e6a87a9aabb7ea07.tar.gz
